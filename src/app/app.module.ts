308413a7c12fb0b4afe763835e15a2df78281624
Aayushi Kumar <WN00121183@WITIN.local>
2021-03-18 15:21:53 +0530

    DragOn Keypress, Click And Toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,13 +9,16 @@ import { routing } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalsComponent } from './modals/modals.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
     ModalsComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     HttpClientModule,
     FormsModule,
--- src/app/modals/modals.component.ts
+++ src/app/modals/modals.component.ts
@@ -1,31 +1,21 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { NgbActiveModal, NgbModal, NgbModalOptions, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-export enum KEY_CODE {
-  UP_ARROW = 24,
-  DOWN_ARROW = 25,
-  RIGHT_ARROW = 26,
-  LEFT_ARROW = 27
-}
-export const ModalConfig: NgbModalOptions = {
-  size: 'lg',
-  backdrop: 'static',
-  keyboard: true,
-}
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-modals',
   templateUrl: './modals.component.html',
   styleUrls: ['./modals.component.css']
 })
 export class ModalsComponent implements OnInit {
-  public modal_count=5;
   public isChecked: boolean = false;
-  public left_weight = 0;
-  public top_weight = 0;
-  constructor(public modalService: NgbModal, private activeModal: NgbActiveModal) { }
+  public modalRef: NgbModalRef;
+  public move_X_axis;
+  public move_Y_axis;
+  constructor(public modalService: NgbModal) { }
 
   ngOnInit(): void {
     var ele = document.getElementById("dragElement");
-    ele.style.display ="none";
+    ele.style.display = "none";
   }
 
   popUp() {
@@ -37,4 +27,45 @@ export class ModalsComponent implements OnInit {
     }
   }
 
+  modalOpen(modal) {
+    this.modalRef = this.modalService.open(modal);
+  }
+  closeModal() {
+    this.modalRef.close();
+  }
+
+  close() {
+    var x = document.getElementById("dragElement");
+    x.style.display = "none";
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log('isChecked',this.isChecked)
+    if (this.isChecked) {
+      if (event.key == "Delete") {
+        this.closeModal();
+        this.close();
+      }
+      var modal_offset = document.getElementById("dragElement");
+      // if (modal_offset.offsetTop > 420) { this.move_Y_axis = 400; }
+      if (modal_offset.offsetLeft < 0) { this.move_X_axis = 0; }
+      if (modal_offset.offsetTop < 0) { this.move_Y_axis = 0; }
+
+      if (modal_offset.offsetLeft >= 0 && modal_offset.offsetTop >= 0) {
+        if (event.key == "ArrowRight") {
+          this.move_X_axis = modal_offset.offsetLeft + 10;
+        }
+        if (event.key == "ArrowLeft") {
+          this.move_X_axis = modal_offset.offsetLeft - 10;
+        }
+        if (event.key == "ArrowUp") {
+          this.move_Y_axis = modal_offset.offsetTop - 10;
+        }
+        if (event.key == "ArrowDown") {
+          this.move_Y_axis = modal_offset.offsetTop + 10;
+        }
+      }
+    }
+  }
 }
